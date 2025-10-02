d32cb79d00afba238a976168a2ce2781869e098c
Brandon Cook <bcook2289@gmail.com>
2025-10-02 14:09:54 +0900

    apis set up

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,20 @@ import dotenv from "dotenv";
 import releaseRoutes from "./routes/releases";
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 app.use("/api/releases", releaseRoutes);
 
+app.get("/", (req, res) => {
+  res.send("running")
+})
+
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
--- src/routes/releases.ts
+++ src/routes/releases.ts
@@ -1,3 +1,11 @@
-const releaseRoutes = () => {}
+import { Router } from "express";
+import {getReleases, updateRelease, deleteRelease, createRelease} from "../controller/releaseController"
 
-export default releaseRoutes;
+const router = Router();
+
+router.get("/", getReleases);
+router.post("/", createRelease);
+router.put("/:id", updateRelease);
+router.delete("/:id", deleteRelease);
+
+export default router;
