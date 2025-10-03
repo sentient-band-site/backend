232cd06198ef40e7e35d597088145a105142c203
Brandon Cook <bcook2289@gmail.com>
2025-10-03 19:38:28 +0900

    feat: authentication complete. Now need to work on Authorization

---
+++ prisma/migrations/20251003041949_add_user_model/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'customer',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
---
+++ src/service/authService.ts
@@ -0,0 +1,93 @@
+import prisma from "../prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { User } from "@prisma/client";
+
+const JWT_SECRET = process.env.JWT_SECRET || "supersecret";
+
+// TODO: findUniqueCheck function
+const findUniqueCheck = async (email: string, password?: string): Promise<User> => {
+    const user = await prisma.user.findUnique({where: {email}})
+    if (!user) throw new Error("User not found");
+    
+    if(password) {
+        isValidCheck(user, password);
+    }
+    return user;
+};
+// TODO: isValidCheck function
+const isValidCheck = async (user: User, password: string) => {
+    const valid = await bcrypt.compare(password, user.password);
+    if(!valid) throw new Error("Invalid email or password")
+};
+// TODO: token function
+const tokenCreate = (user: User) => {
+    return jwt.sign(
+        {id: user.id, role: user.role},
+        process.env.JWT_SECRET as string,
+        {expiresIn: "1d"}
+    );
+};
+
+export const registerUser = async (data: { email: string; password: string; }) => {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const user = await prisma.user.create({
+        data: {
+            email: data.email,
+            password: hashedPassword,
+            role: "customer",
+        },
+    })
+
+    return { id: user.id, email:user.email, role: user.role };
+};
+
+export const loginUser = async (email: string, password: string) => {
+    const user = await findUniqueCheck(email, password);
+    const token = tokenCreate(user)
+
+    // const user = await prisma.user.findUnique({ where: { email } });
+    // if (!user) throw new Error("User not found");
+
+    // const valid = await bcrypt.compare(password, user.password);
+    // if(!valid) throw new Error("Invalid email or password");
+
+
+    // const token = jwt.sign(
+    //     {userId: user.id, role: user.role }, 
+    //     JWT_SECRET, 
+    //     { expiresIn: "1d"}
+    // )
+
+    return { token, user };
+};
+
+// DEV ONLY
+
+export const deleteUser = async (email: string) => {
+    const user = await findUniqueCheck(email);
+    // const user = await prisma.user.findUnique({ where: { email }})
+    // if(!user) throw new Error("User not found");
+
+    return await prisma.user.delete({where: { email }});
+};
+
+export const getToken = async (email: string, password: string) => {
+    const user = await findUniqueCheck(email, password);
+    const token = tokenCreate(user);
+
+    // const user = await prisma.user.findUnique({where: {email}});
+    // if(!user) throw new Error("User not found");
+
+
+    // const valid = await bcrypt.compare(password, user.password);
+    // if(!valid) throw new Error("Invalid email or password");
+    // const token = jwt.sign(
+    //     {id: user.id, role: user.role},
+    //     process.env.JWT_SECRET as string,
+    //     {expiresIn: "1d"}
+    // );
+
+    return token;
+};
